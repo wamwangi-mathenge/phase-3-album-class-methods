a932f43246e4b09414477cebcb3449597c586d9f
Brian <wamwangimathenge@gmail.com>
2023-02-14 17:04:43 +0300

    defining the class method

--- run.rb
+++ run.rb
@@ -4,17 +4,23 @@ class Album
 
     @@album_count = 0
 
-    # Setter method
-    def release_date=(date)
-        @release_date = date
+    def self.count
+        @@album_count
     end
 
-    #Getter method
-    def release_date
-        @release_date
-    end
+    # # Setter method
+    # def release_date=(date)
+    #     @release_date = date
+    # end
+
+    # #Getter method
+    # def release_date
+    #     @release_date
+    # end
 end
 
-album = Album.new
-album.release_date = 1991
-puts album.release_date
+# album = Album.new
+# album.release_date = 1991
+# puts album.release_date
+
+puts Album.count
