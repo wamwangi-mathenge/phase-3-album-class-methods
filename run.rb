92a2b5d7825054c8838656d387294788dbc978f8
Brian <wamwangimathenge@gmail.com>
2023-02-14 17:07:10 +0300

    Operating on the class variable

--- run.rb
+++ run.rb
@@ -4,6 +4,10 @@ class Album
 
     @@album_count = 0
 
+    def initialize
+        @@album_count += 1
+    end
+
     def self.count
         @@album_count
     end
@@ -22,5 +26,11 @@ end
 # album = Album.new
 # album.release_date = 1991
 # puts album.release_date
+
+# puts Album.count
+
+Album.new
+Album.new
+Album.new
 
 puts Album.count
